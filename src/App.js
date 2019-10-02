fa5eca58eddad1f4a60a56dc102145b2bae7c8ad
Ashley <ashkwo1996@gmail.com>
2019-10-01 17:45:23 -0700

    added ip api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: {}
+    }
+  }
+  
+  componentDidMount() {
+    axios.get('https://ipapi.co/json/')
+      .then(res => {
+        this.setState({ location : res.data });
+        console.log(res.data)
+      });
+
+    const API_KEY = '29b741d85079c9ed7a103d693993129a';
+    axios.get(`https://api.darksky.net/forecast/${API_KEY}/34.0121,-117.6883`)
+      .then(res => {
+        console.log(res.data)
+      });
+    /* Geolocation API 
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          console.log(position.coords.latitude, position.coords.longitude)
+          this.setState({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          })
+        },
+        err => {
+          console.warn(`ERROR ${err.code}: ${err.message}`);
+        }
+      );
+    }*/
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.location.latitude}, {this.state.location.longitude}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
