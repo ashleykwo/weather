7f3afa37c20e394de165e0d9a356ba354b61b436
Ashley <ashkwo1996@gmail.com>
2019-11-01 14:56:19 -0700

    formatted hourly time

--- src/components/HourlyForecast/HourlyForecast.js
+++ src/components/HourlyForecast/HourlyForecast.js
@@ -5,16 +5,15 @@ import './HourlyForecast';
 const hourlyForecast = (props) => {
   
   console.log(props.hourlyForecast);
-  
 
   return(
     <div className="container">
       {
         Array.isArray(props.hourly) && props.hourly.map((item, index) => 
-          <div>
-            <p key={index}>{item.time}</p>
-            <p>{item.temperature}</p>
+          <div key={`hour-${index}`} className='hour'>
             <WeatherIcons weather={item.icon}></WeatherIcons>
+            <p>{new Date(item.time * 1000).toLocaleTimeString("en-US", { timeZone: props.timezone, hour12: false } )}</p>
+            <p>{item.temperature}</p>
           </div>
         )
       }
